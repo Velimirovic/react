602ad5a221e61017623bff29171ccd3b5a91b494
Velimirovic <velimirovitsh@gmail.com>
2021-12-01 01:11:47 +0100

    starting files + nav

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Menu = () => {
+    return (
+        <div>
+            <p> Welcome. </p>
+        </div>
+    );
+};
+export default Menu;
---
+++ src/components/Order/Order.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Order = () => {
+    return (
+        <div>
+            <p> Welcome. </p>
+        </div>
+    );
+};
+export default Order;
