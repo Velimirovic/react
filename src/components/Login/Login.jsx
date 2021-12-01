da1d7b2b7135cced82466a83852fba6ea37c6999
Velimirovic <velimirovitsh@gmail.com>
2021-12-01 19:07:31 +0100

    xd

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Link } from "react-router-dom";
 import "./Login.css";
 
 const Login = () => {
-
     /*const[email,setEmail] = useState("");
     const[password,setPassword] = useState("");
     const addNewLogin = () => {
@@ -17,7 +16,6 @@ const Login = () => {
 
     };*/
 
-
     return (
         <div>
             <main>
@@ -25,20 +23,18 @@ const Login = () => {
                     <div className='d-flex justify-content-center'>
                         <div className='titles'>
                             <h2 className='text-color text-center text-uppercase mt-4 animate__animated animate__fadeInDown' style={{ animationDuration: "2s" }}>
-                                Logina
+                                Login
                             </h2>
                         </div>
                     </div>
                     <div className='order-div animate__animated animate__fadeInLeft' style={{ animationDuration: "2s" }}>
-                        
-                        
                         <form>
                             <div className='form-group row mt-3'>
                                 <label for='inputEmail3' className='col-sm-4 col-md-4 col-lg-2 col-form-label sd'>
                                     Email
                                 </label>
                                 <div className='col-sm-10'>
-                                    <input  className='form-control'/>
+                                    <input className='form-control' />
                                 </div>
                             </div>
                             <div className='form-group row mt-3'>
@@ -46,7 +42,7 @@ const Login = () => {
                                     Password
                                 </label>
                                 <div className='col-sm-10'>
-                                    <input className='form-control' placeholder='Password' type="password"/>
+                                    <input className='form-control' placeholder='Password' type='password' />
                                 </div>
                             </div>
                             <div className='form-group row mt-4'>
@@ -60,10 +56,6 @@ const Login = () => {
                                 <Link to='/Register'> Create account </Link>
                             </div>
                         </form>
-
-
-
-
                     </div>
                 </div>
             </main>
